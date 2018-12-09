7162e445a2421337563f3a2a1d14522311b602f1
Jimzy <JimzyLui@users.noreply.github.com>
2018-12-09 12:52:40 -1000

    finished descendingOrder

--- descending-order/descendingOrder.js
+++ descending-order/descendingOrder.js
@@ -1,3 +1,19 @@
 module.exports = function descendingOrder(number) {
   // write code in here
+  console.log("type: " + typeof number);
+  if (typeof number !== "number") {
+    return "not a number!";
+  }
+  // cast as string... toString()
+  // conv to array... split('')
+  // sort backward... sort(fx) //w/ fx b-a
+  // join into string... join('')
+  // convert back to int... parseInt()
+
+  var arr = number.toString().split("");
+  var sortRev = (a, b) => {
+    return b - a;
+  };
+  var arrR = arr.sort(sortRev);
+  return parseInt(arrR.join(""));
 };
