01f543c6625fe00400f0d605b9db66d8bb2f9729
Jimzy <JimzyLui@users.noreply.github.com>
2018-12-09 20:06:32 -1000

    finished findMiddleValue

--- find-middle-value/findMiddleValue.js
+++ find-middle-value/findMiddleValue.js
@@ -1,3 +1,15 @@
 module.exports = function findMiddleValue(numArray) {
   // write your code in here
-}
+  /*
+    Plan:
+    make a copy of the array and sort it
+    Find the middle number (at index 1)
+    find the index of that number in the original array
+  */
+  var sortAscAlg = (a, b) => {
+    return a - b;
+  };
+  var arrSort = numArray.slice(0).sort(sortAscAlg);
+  var middleVal = arrSort[1];
+  return numArray.indexOf(middleVal);
+};
