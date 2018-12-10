95786a88037b31913b146e0646199dc8c04faa45
Jimzy <JimzyLui@users.noreply.github.com>
2018-12-09 19:53:36 -1000

    finished evenLadderPattern

--- even-ladder-pattern/evenLadderPattern.js
+++ even-ladder-pattern/evenLadderPattern.js
@@ -1,3 +1,14 @@
 module.exports = function evenLadderPattern(num) {
   // write your code in here
-}
+  if (typeof num !== "number") {
+    return "";
+  }
+  if (num < 2) {
+    return "";
+  }
+  var str = "";
+  for (var x = 2; x <= num; x += 2) {
+    str += x.toString().repeat(x) + "\n";
+  }
+  return str.trim();
+};
