37db52722a6b407303f4ce75a274d712b68d68a8
Jimzy <JimzyLui@users.noreply.github.com>
2018-12-09 20:45:00 -1000

    finished findTheLongestWord

--- find-the-longest-word/findTheLongestWord.js
+++ find-the-longest-word/findTheLongestWord.js
@@ -1,3 +1,11 @@
 module.exports = function findTheLongestWord(sen) {
   // write your code in here
-}
+  /*
+  Plan:
+   use .reduce comparing the string length
+*/
+  var reducer = (longest, word) => {
+    return longest.length > word.length ? longest : word;
+  };
+  return sen.reduce(reducer);
+};
