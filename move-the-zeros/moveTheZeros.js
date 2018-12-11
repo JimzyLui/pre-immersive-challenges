e61d0fc007b79d63425774297a4387fba2278b21
Jimzy <JimzyLui@users.noreply.github.com>
2018-12-10 20:06:56 -1000

    finished moveTheZeros

--- move-the-zeros/moveTheZeros.js
+++ move-the-zeros/moveTheZeros.js
@@ -1,3 +1,29 @@
 module.exports = function moveTheZeros(arrNum, isRight) {
   // write your code here
-}
+  /*
+  Plan:
+  count the zeros while pushing the number into an empty array
+  then add the zeros into the new array depending on location
+  */
+  "use strict";
+  var arr = [];
+  var zeroCtr = 0;
+  for (var i = 0; i < arrNum.length; i++) {
+    if (arrNum[i] === 0) {
+      zeroCtr++;
+    } else {
+      arr.push(arrNum[i]);
+    }
+  }
+  // console.log("zeroCtr: " + zeroCtr);
+
+  var arrZ = new Array(zeroCtr).fill(0);
+
+  // console.log("arrZ: " + arrZ);
+
+  if (isRight) {
+    return arr.concat(arrZ);
+  } else {
+    return arrZ.concat(arr);
+  }
+};
