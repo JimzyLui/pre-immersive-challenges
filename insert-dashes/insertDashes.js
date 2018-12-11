1968cd4c902d2cb0235b809ccd54c186f5cf2fed
Jimzy <JimzyLui@users.noreply.github.com>
2018-12-10 18:53:56 -1000

    finished insertDashes

--- insert-dashes/insertDashes.js
+++ insert-dashes/insertDashes.js
@@ -1,3 +1,16 @@
 module.exports = function insertDashes(num) {
   // write your code in here
-}
+  var str = num.toString();
+  var result = str[0];
+  var isEven = x => {
+    return x % 2 == 0;
+  };
+  for (var i = 0; i < str.length - 1; i++) {
+    if (isEven(str[i]) && isEven(str[i + 1])) {
+      result += "-" + str[i + 1];
+    } else {
+      result += str[i + 1];
+    }
+  }
+  return result;
+};
