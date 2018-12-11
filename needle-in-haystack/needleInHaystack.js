d98f370f2fc6f6517d6a9c9638789da9bc5f6f67
Jimzy <JimzyLui@users.noreply.github.com>
2018-12-10 20:12:12 -1000

    finished needleInHaystack

--- needle-in-haystack/needleInHaystack.js
+++ needle-in-haystack/needleInHaystack.js
@@ -1,3 +1,9 @@
 module.exports = function needleInHaystack(haystack) {
   // write code in here
-}
+  var index = haystack.indexOf("needle");
+  if (index < 0) {
+    return false;
+  } else {
+    return index;
+  }
+};
