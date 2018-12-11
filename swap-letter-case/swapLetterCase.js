2ff1a295b172397c10f28cdb6eabd524375ec6bd
Jimzy <JimzyLui@users.noreply.github.com>
2018-12-10 21:22:22 -1000

    finished swapLetterCase

--- swap-letter-case/swapLetterCase.js
+++ swap-letter-case/swapLetterCase.js
@@ -1,3 +1,24 @@
 module.exports = function swapLetterCase(str) {
   // write your code in here
-}
+  var isLetter = s => {
+    return s.match(/[A-Za-z]/);
+  };
+  var isUpper = s => {
+    return s.match(/[A-Z]/);
+  };
+
+  return str
+    .split("")
+    .map(x => {
+      if (isLetter(x)) {
+        if (isUpper(x)) {
+          return x.toLowerCase();
+        } else {
+          return x.toUpperCase();
+        }
+      } else {
+        return x;
+      }
+    })
+    .join("");
+};
