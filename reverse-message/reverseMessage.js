14f1bdc7657791a3cc00741bc5cbc0fe7115a85f
Jimzy <JimzyLui@users.noreply.github.com>
2018-12-10 20:59:17 -1000

    finished reverseMessage

--- reverse-message/reverseMessage.js
+++ reverse-message/reverseMessage.js
@@ -1,3 +1,22 @@
 module.exports = function reverseMessage(str) {
   // write your code in here
-}
+  /*
+    Plan:
+    split into array
+    map lowercase, backwards, cap
+    join with spaces
+  */
+  var result = str
+    .split(" ")
+    .map(x => x.toLowerCase())
+    .reverse()
+    .map(x =>
+      x
+        .split("")
+        .reverse()
+        .join("")
+    )
+    .map(x => x.charAt(0).toUpperCase() + x.slice(1))
+    .join(" ");
+  return result;
+};
