c270664a0bf21f504fb8caca7c939758176e9e9a
Jimzy <JimzyLui@users.noreply.github.com>
2018-12-10 18:34:14 -1000

    finished halflife

--- half-life/halfLife.js
+++ half-life/halfLife.js
@@ -1,3 +1,10 @@
 module.exports = function halfLife(initialQuantity, quantityRemaining, time) {
-  
-}
+  var halfLifeCtr = 0;
+  var qty = initialQuantity;
+
+  while (qty > quantityRemaining) {
+    qty /= 2;
+    halfLifeCtr++;
+  }
+  return time / halfLifeCtr;
+};
