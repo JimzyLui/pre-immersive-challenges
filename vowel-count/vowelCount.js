f33dc7cdb28400e9408c824e2241cc1b7dc9e1d0
Jimzy <JimzyLui@users.noreply.github.com>
2018-12-10 21:37:10 -1000

    finished vowelCount

--- vowel-count/vowelCount.js
+++ vowel-count/vowelCount.js
@@ -1,3 +1,8 @@
 module.exports = function vowelCount(str) {
   // write your code in here
-}
+  var isVowel = s => {
+    return s.match(/[aeiou]/);
+  };
+
+  return str.split("").filter(isVowel).length;
+};
