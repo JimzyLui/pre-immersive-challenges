efb824c2d85a3fbebcd8866b05e4b604fef750fe
Jimzy <JimzyLui@users.noreply.github.com>
2018-12-10 17:29:16 -1000

    finished findTheStrayValue

--- find-the-stray-value/findTheStrayValue.js
+++ find-the-stray-value/findTheStrayValue.js
@@ -1,3 +1,22 @@
 module.exports = function findTheStrayValue(array) {
   // write your code in here
-}
+  var v1 = array[0];
+  var v1Ctr = 1;
+  var v2 = "";
+  var v2Ctr = 0;
+  //compare to v1
+  for (var i = 1; i < array.length; i++) {
+    if (array[i] === v1) {
+      v1Ctr++;
+    } else {
+      v2 = array[i];
+      v2Ctr++;
+    }
+  }
+
+  if (v1Ctr === 1) {
+    return v1;
+  } else {
+    return v2;
+  }
+};
